952741fab344160064bb5ac935ea82aa2ba87092
disection <diseccionmexico@gmail.com>
2022-07-21 19:53:32 -0500

    CHANGE List/index.js to view to diferents list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-//import users from './assets/mock_users'
+import users from './assets/mock_users'
 import products from './assets/mock_products'
 import List from './Componets/List'
  
 function App() {
   return (
     <div className="App">  
-      {/* <List data={users} />       */}
-      <List data={products} />
+      <List data={users} type='listaDeUsuarios'/>
+      <List data={products} type='listaDeProductos' />
     </div>
   )
 }
--- src/Componets/List/index.js
+++ src/Componets/List/index.js
@@ -1,11 +1,19 @@
 import ProductCard from '../ProductsCard'
+import UserCard from '../UserCard'
 
 const List = (props) => {
-    const {data} = props
+    const {data, type} = props
+   const selectCardType = (type, cardInfo) =>{
+    const cardsMap = {
+      listaDeProductos: <ProductCard cardData={cardInfo}/>,
+      listaDeUsuarios: <UserCard cardData={cardInfo}/>
+    }
+    return cardsMap[type]
+   }
     return (
         <div className="row row-cols-1 row-cols-md-4 g-4 ">
         { data.map( ( item,index ) => 
-        <ProductCard cardData = {item}/>) }
+        selectCardType(type, item)) }
       </div>
       
     )
