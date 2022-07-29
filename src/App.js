0103fd785cdb9bc97fedc0f461eacb98cc132b1e
disection <diseccionmexico@gmail.com>
2022-07-29 11:56:26 -0500

    MODULE ItemList and make state survey

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState } from 'react';
 import './App.css';
 import products from './mocks/products';
+import ItemList from './components/ItemList';
 
  
 function App() { 
@@ -9,6 +10,7 @@ function App() {
   const [ selectedProducts, setSelectedProducts ] = useState([])
 
   // handler selected with id product from products
+  
    const addProduct = (id) =>{
     console.log(id)
     // brought the object
@@ -23,36 +25,13 @@ function App() {
         <div className="row">
           <div className="col-12 col-md-6">
             <h1>Catálogo de productos</h1>
-            <div className="row row-cols-1 row-cols-md-3 gap-3">
-              {
-              // extract objects from mock and make cards products
-              products.map( (product) => {
-                // destruct object product
-                const {id,title,price,description,rating, image}= product
-                return (
-                  <div className="col" key={id}>
-                    <div className="card h-100">
-                      <img src={image} className='w-100' alt="" />
-                      <h2 className="card-body">{title}</h2>
-                      <p className="card-text">{description.substring(0,40)/* only see a part from description */}</p>
-                      <p className="card-text">{rating.rate}</p>
-                      <p className="card-text">{price}</p>
-                      <button 
-                      type="button" 
-                      className="btn btn-primary" 
-                      /* para agregar un parametro externo al handler se debe encapsular en una funtion por el id */
-                      onClick={ () => {addProduct(id)} }>
-                        Agregar al carrito</button>
-                    </div>
-                  </div>
-                )
-              })}
-              
-            </div>
+            <ItemList 
+            productsArray={products} 
+            // variable is sent as a prop so that it can do a state survey when modulating the list
+            addProductHandler={addProduct}/>
           </div>
           <div className="col-12 col-md-6">
-            <h1>Carrito de compras</h1>
-            
+            <h1>Carrito de compras</h1>            
               {
                 // conditional rendering if selectedProducta is empty
                 !selectedProducts.length ? <h2>Selecciona un producto de la lista</h2>:
